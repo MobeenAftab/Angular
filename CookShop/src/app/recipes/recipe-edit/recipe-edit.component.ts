67a199d62887b3010a5cc88550ac2727c5c42add
Mobeen Aftab <mobeenaftab1@gmail.com>
2017-12-13 16:55:06 +0000

    CookShop
    Edit recipe control

--- CookShop/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ CookShop/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { RecipeService } from '../recipe-service.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -10,8 +12,9 @@ export class RecipeEditComponent implements OnInit {
 
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.route.params.subscribe(
@@ -19,9 +22,32 @@ export class RecipeEditComponent implements OnInit {
         this.id = +params['id'];
         // If no id returned, in new mode
         this.editMode = params['id'] != null;
+        this.initFornm();
         console.log(this.editMode);
       }
     );
+  }
+
+  private initFornm() {
+    let recipeName = '';
+    let recipeImg = '';
+    let recipeDesc = '';
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImg = recipe.imagePath;
+      recipeDesc = recipe.description;
+    }
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imgPath' : new FormControl(recipeImg),
+      'description' : new FormControl(recipeDesc)
+    })
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
   }
 
 }
