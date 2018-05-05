6f5fccf8a3d03beeab8930c10350a6dfec281924
Mobeen Aftab <mobeenaftab1@gmail.com>
2018-05-05 03:28:24 +0100

    tour-of-heros
    Hero detail component

--- angular-tour-of-heros/src/app/app.module.ts
+++ angular-tour-of-heros/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HerosComponent } from './components/heros/heros.component';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HerosComponent
+    HerosComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-tour-of-heros/src/app/components/hero-detail/hero-detail.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Hero } from '../heros/hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  // Recive hero from heros component
+  @Input() hero: Hero;
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('Display Hero Details');
+  }
+
+}
